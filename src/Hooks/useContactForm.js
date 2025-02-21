813327891b563d3e431d34738370e5b926ba884f
Nicolas HECKER <hecker.nicolas.pro@gmail.com>
2025-02-21 17:19:27 +0100

    Feat: Implement contact form with email sending functionality using EmailJS

--- src/Hooks/useContactForm.js
+++ src/Hooks/useContactForm.js
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import emailjs from "emailjs-com";
 
 const useContactForm = () => {
+  const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+  const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+  const userId = import.meta.env.VITE_EMAILJS_USER_ID;
   const { t, i18n } = useTranslation();
   const [formData, setFormData] = useState({
     name: "",
@@ -30,8 +34,25 @@ const useContactForm = () => {
       return;
     }
 
-    setSuccess(t("contactSuccess"));
-    setFormData({ name: "", email: "", message: "" });
+    // Utiliser EmailJS pour envoyer l'email
+    emailjs
+      .send(
+        serviceId, // Ton ID de service EmailJS
+        templateId, // Ton ID de modèle d'email
+        formData, // Les données que tu veux envoyer (nom, email, message)
+        userId
+      )
+      .then(
+        (result) => {
+          console.log("Message envoyé:", result.text);
+          setSuccess(t("contactSuccess"));
+          setFormData({ name: "", email: "", message: "" });
+        },
+        (error) => {
+          console.error("Erreur lors de l'envoi de l'email:", error.text);
+          setError(t("contactError"));
+        }
+      );
   };
 
   return { formData, handleChange, handleSubmit, error, success };
